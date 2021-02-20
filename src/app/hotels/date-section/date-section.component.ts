015ca060c0a5b1b2d48cc0fa7bdd4585df068d94
maha <mahaazzam88@gmail.com>
2021-02-20 23:52:36 +0200

    subtract date

--- src/app/hotels/date-section/date-section.component.ts
+++ src/app/hotels/date-section/date-section.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { type } from 'os';
+import { HotelService } from 'src/app/_services/hotel.service';
 
 @Component({
   selector: 'app-date-section',
@@ -7,13 +9,35 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./date-section.component.scss'],
 })
 export class DateSectionComponent implements OnInit {
+  checkIn = new Date();
+  checkOut = new Date();
+  subtractDates = 0;
+  days = 0;
   inputEvent(event) {
-    console.log(event.value);
+
+    if(event.target._elementRef.nativeElement.alt == "checkIn"){
+      this.checkIn = event.value;
+      console.log(this.checkIn);
+    } else if (event.target._elementRef.nativeElement.alt == "checkOut"){
+      this.checkOut = event.value;
+      console.log(this.checkOut);
+    }
+    else{
+      console.log('neither')
+    }
+    
+    // To calculate the time difference of two dates 
+    this.subtractDates = this.checkOut.getTime() - this.checkIn.getTime(); 
+    // To calculate the no. of days between two dates 
+    this.days = this.subtractDates / (1000 * 3600 * 24); 
+    console.log(this.days);
+    
   }
   changeEvent(event) {
-    console.log(event.value);
+    // console.log(event.value);
   }
-  constructor() {}
+  constructor(private hotelService: HotelService) {}
 
   ngOnInit(): void {}
+  
 }
