b044f714bf449c950525df70f8bed3f3e999ca92
Lama Mohamed <74375170+LamaMohamed@users.noreply.github.com>
2021-02-21 17:13:48 +0200

    update up till now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HotelComponentComponent } from './hotels/hotel-component/hotel-component.component';
 import { HotelsListingComponent } from './hotels/hotels-listing/hotels-listing.component';
-import { HomeComponentComponent } from './home/home-component/home-component.component';
 import { HomeListingComponent } from './home/home-listing/home-listing.component';
+import { CardComponent } from './home/card/card.component';
+import { ButtonComponent } from './home/button/button.component';
+import { SearchPipe } from './pipes/search.pipe';
+/* import { MDBBootstrapModule } from 'angular-bootstrap-md'; */
 
 @NgModule({
   declarations: [
     AppComponent,
     HotelComponentComponent,
     HotelsListingComponent,
-    HomeComponentComponent,
-    HomeListingComponent
+    HomeListingComponent,
+    CardComponent,
+    ButtonComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/card/card.component.spec.ts
+++ src/app/home/card/card.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponentComponent } from './home-component.component';
+import { CardComponent } from './card.component';
 
-describe('HomeComponentComponent', () => {
-  let component: HomeComponentComponent;
-  let fixture: ComponentFixture<HomeComponentComponent>;
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponentComponent ]
+      declarations: [ CardComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponentComponent);
+    fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
