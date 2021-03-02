a49913d930519fe5dd33d9689e1fcf9530156f50
maha <mahaazzam88@gmail.com>
2021-03-03 01:19:12 +0200

    hotel-listing model

--- src/app/home/home-listing/home-listing.component.ts
+++ src/app/home/home-listing/home-listing.component.ts
@@ -1,66 +1,80 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef ,NgModule,Renderer2} from '@angular/core';
-import {Home} from "../../_model/home/home";
-import {HomeService} from "../../_services/home/home.service";
-import {DreamTripService} from "../../_services/home/dream-trip.service";
-import {DreamTripCards} from '../../_model/home/dreamTrip';
-import {MoreExploreService} from "../../_services/home/more-explore.service";
-import {MoreToExplore} from '../../_model/home/more-explore'
-
-
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  NgModule,
+  Renderer2,
+} from '@angular/core';
+import { Home } from '../../_model/home/home';
+import { HomeService } from '../../_services/home/home.service';
+import { DreamTripService } from '../../_services/home/dream-trip.service';
+import { DreamTripCards } from '../../_model/home/dreamTrip';
+import { MoreExploreService } from '../../_services/home/more-explore.service';
+import { MoreToExplore } from '../../_model/home/more-explore';
+import { HotelService } from 'src/app/_services/hotels/hotel.service';
+import { Hotel } from 'src/app/_model/hotels/hotel';
 
 @Component({
   selector: 'app-home-listing',
   templateUrl: './home-listing.component.html',
-  styleUrls: ['./home-listing.component.scss']
+  styleUrls: ['./home-listing.component.scss'],
 })
 export class HomeListingComponent implements OnInit {
-   dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
+  dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
 
-   top : DreamTripCards[];
-   headlines : MoreToExplore[];
-   cards:Home[];
-   isToggel=false;
-   search:string[] =[];
-   current:0;
-   size:2;
-   slides: any = [[]];
+  top: DreamTripCards[];
+  headlines: MoreToExplore[];
+  cards: Home[];
+  isToggel = false;
+  search: string[] = [];
+  current: 0;
+  size: 2;
+  slides: any = [[]];
 
-  constructor(private renderer: Renderer2,
-    private dreamTripSerivce:DreamTripService,
-    private moreExploreSerivce:MoreExploreService,
-    private homeService:HomeService) { }
+  hotels : Hotel[]=[];
+
+  constructor(
+    private renderer: Renderer2,
+    private dreamTripSerivce: DreamTripService,
+    private moreExploreSerivce: MoreExploreService,
+    private homeService: HomeService,
+    private hotelService :HotelService,
+  ) {}
 
   ngOnInit() {
-     this.top=this.dreamTripSerivce.getTop();
-     this.headlines=this.moreExploreSerivce.getHeadline();
-      this.cards=this.homeService.getCard();
-      this.slides = this.chunk(this.cards, 4);
-
+    this.top = this.dreamTripSerivce.getTop();
+    this.headlines = this.moreExploreSerivce.getHeadline();
+    this.cards = this.homeService.getCard();
+    this.slides = this.chunk(this.cards, 4);
   }
 
-  myFunction(){
-    this.isToggel= !(this.isToggel)
-    console.log("hellooooo")
+  myFunction() {
+    this.isToggel = !this.isToggel;
+    console.log('hellooooo');
     return this.isToggel;
-}
-    
-/*  getSlicedProducts():Home[]{
+  }
+
+  /*  getSlicedProducts():Home[]{
   const start= this.current* this.size;
   
   return this.cards.slice(start,start+this.size);
   
 }  */
 
-// onSearchHandler(searchInput){
-//   console.log(searchInput.value);
-//   this.search.push(searchInput.value);
-//   console.log(this.search)
-//     return this.search;
-// }
+  // onSearchHandler(searchInput){
+  //   console.log(searchInput.value);
+  //   this.search.push(searchInput.value);
+  //   console.log(this.search)
+  //     return this.search;
+  // }
 
-timeout: any = null;
+  timeout: any = null;
 
- onKeySearch(event: any) {
+  onKeySearch(event: any) {
     clearTimeout(this.timeout);
     var $this = this;
     this.timeout = setTimeout(function () {
@@ -70,28 +84,40 @@ timeout: any = null;
     }, 1);
   }
 
- executeListing(value: string) {
+  executeListing(value: string) {
     // alert(value);
-    console.log(value)
-    if (value !== ''){
+    console.log(value);
+    if (value !== '') {
       this.search.push(value);
     }
   }
 
-chunk(arr: any, chunkSize: number) {
-  let R = [];
-  for (let i = 0, len = arr.length; i < len; i += chunkSize) {
-    R.push(arr.slice(i, i + chunkSize));
+  chunk(arr: any, chunkSize: number) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
   }
-  return R;
-}
-ngAfterViewInit() {
-  const buttons = document.querySelectorAll('.btn-floating');
-  buttons.forEach((el: any) => {
-    this.renderer.removeClass(el, 'btn-floating');
-    this.renderer.addClass(el, 'px-3');
-    this.renderer.addClass(el.firstElementChild, 'fa-3x');
-  });
-}
+  ngAfterViewInit() {
+    const buttons = document.querySelectorAll('.btn-floating');
+    buttons.forEach((el: any) => {
+      this.renderer.removeClass(el, 'btn-floating');
+      this.renderer.addClass(el, 'px-3');
+      this.renderer.addClass(el.firstElementChild, 'fa-3x');
+    });
+  }
 
+  city : string;
+  getSearchHotels(hotelValue) {
+    this.city = hotelValue.toLowerCase();
+    console.log(this.city);
+  //  this.hotelService.getAllHotels().subscribe((resp) => {
+  //   Object.values(resp).map((res) => {
+  //     console.log(res);
+  //     this.hotels.push(res);
+  //   });
+  //    console.log(this.hotels);
+  // });
   }
+}
